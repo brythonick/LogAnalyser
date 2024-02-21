6c5897e3cca75433ce3abf083828aa195cfc5467
Nick Farnham <brythonick@gmail.com>
2024-02-21 22:42:08 +0000

    Add simple validation for date parts
    Some sanity checking to provide a way to catch and handle any bad dates
    later, while reading the stream or the file.

--- LogAnalyser/LogDate.cs
+++ LogAnalyser/LogDate.cs
@@ -2,15 +2,60 @@
 {
     public class LogDate
 	{
-		public int day { get; set; }
-		public int month { get; set; }
-        public int year { get; set; }
+		private uint _day;
+		public uint day
+		{
+			get { return _day; }
+			set
+			{
+				if (value == 0)
+					throw new DateDayZero();
+				else if (value > 31)
+					throw new DateDayTooLarge();
+				else
+					_day = value;
+			}
+		}
+
+		private uint _month;
+		public uint month
+		{
+			get { return _month; }
+			set
+			{
+				if (value == 0)
+					throw new DateMonthZero();
+				else if (value > 12)
+					throw new DateMonthTooLarge();
+				else
+					_month = value;
+			}
+		}
+
+		private uint _year;
+        public uint year
+		{
+			get { return _year; }
+			set
+			{
+				if (value < 1970)
+					throw new DateYearTooSmall();
+				else
+					_year = value;
+			}
+		}
 
 		public LogDate() { }
-        public LogDate(int argDay, int argMonth, int argYear)
+        public LogDate(uint argDay, uint argMonth, uint argYear)
 		{
 			day = argDay; month = argMonth; year = argYear;
 		}
 	}
+
+	public class DateDayZero : Exception { }
+    public class DateDayTooLarge : Exception { }
+	public class DateMonthZero : Exception { }
+    public class DateMonthTooLarge : Exception { }
+    public class DateYearTooSmall : Exception { }
 }
 
--- LogAnalyserTests/DateClassTests.cs
+++ LogAnalyserTests/DateClassTests.cs
@@ -35,4 +35,75 @@ public class DateClassTests
         });
         Assert.Null(exception);
     }
+
+    [Fact]
+    public void validationRejectsDayTooSmall()
+    {
+        Assert.Throws<DateDayZero>(() => date.day = 0);
+    }
+
+    [Fact]
+    public void validationRejectsDayTooLarge()
+    {
+        Assert.Throws<DateDayTooLarge>(() => date.day = 32);
+        Assert.Throws<DateDayTooLarge>(() => date.day = 40);
+        Assert.Throws<DateDayTooLarge>(() => date.day = 2000);
+    }
+
+    [Fact]
+    public void validationRejectsMonthTooSmall()
+    {
+        Assert.Throws<DateMonthZero>(() => date.month = 0);
+    }
+
+    [Fact]
+    public void validationRejectsMonthTooLarge()
+    {
+        Assert.Throws<DateMonthTooLarge>(() => date.month = 13);
+        Assert.Throws<DateMonthTooLarge>(() => date.month = 14);
+        Assert.Throws<DateMonthTooLarge>(() => date.month = 50);
+    }
+
+    [Fact]
+    public void validationRejectsYearTooSmall()
+    {
+        Assert.Throws<DateYearTooSmall>(() => date.year = 0);
+        Assert.Throws<DateYearTooSmall>(() => date.year = 1);
+        Assert.Throws<DateYearTooSmall>(() => date.year = 1492);
+        Assert.Throws<DateYearTooSmall>(() => date.year = 1969);
+    }
+
+    [Fact]
+    public void validationAllowsYearAfter1970()
+    {
+        Exception exception = Record.Exception(() => date.year = 1970);
+        Assert.Null(exception);
+        exception = Record.Exception(() => date.year = 2023);
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void validationAppliesToArgs()
+    {
+        Assert.Throws<DateDayZero>(() =>
+        {
+            LogDate d = new LogDate(0, 1, 2023);
+        });
+        Assert.Throws<DateDayTooLarge>(() =>
+        {
+            LogDate d = new LogDate(40, 1, 2023);
+        });
+        Assert.Throws<DateMonthZero>(() =>
+        {
+            LogDate d = new LogDate(1, 0, 2023);
+        });
+        Assert.Throws<DateMonthTooLarge>(() =>
+        {
+            LogDate d = new LogDate(1, 13, 2023);
+        });
+        Assert.Throws<DateYearTooSmall>(() =>
+        {
+            LogDate d = new LogDate(1, 1, 1969);
+        });
+    }
 }
