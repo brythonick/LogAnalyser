1302c9c654c003df557270d707e04af222899797
Nick Farnham <brythonick@gmail.com>
2024-02-21 22:00:41 +0000

    Add basic features of custom log date class

---
+++ LogAnalyser/LogDate.cs
@@ -0,0 +1,16 @@
+﻿namespace LogAnalyser
+{
+    public class LogDate
+	{
+		public int day { get; set; }
+		public int month { get; set; }
+        public int year { get; set; }
+
+		public LogDate() { }
+        public LogDate(int argDay, int argMonth, int argYear)
+		{
+			day = argDay; month = argMonth; year = argYear;
+		}
+	}
+}
+
---
+++ LogAnalyserTests/DateClassTests.cs
@@ -0,0 +1,38 @@
+﻿using LogAnalyser;
+
+namespace LogAnalyserTests;
+
+public class DateClassTests
+{
+    private LogDate date = new LogDate();
+
+    [Fact]
+    public void holdsDay()
+    {
+        Exception exception = Record.Exception(() => { date.day = 1; });
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void holdsMonth()
+    {
+        Exception exception = Record.Exception(() => { date.month = 1; });
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void holdsYear()
+    {
+        Exception exception = Record.Exception(() => { date.year = 2023; });
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void takesDatePartsAtInit()
+    {
+        Exception exception = Record.Exception(() => {
+            LogDate d = new LogDate(1, 1, 2023);
+        });
+        Assert.Null(exception);
+    }
+}
