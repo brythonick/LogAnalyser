8624945ccc32e5f1350e2ce406ab0855ce5a1a80
Nick Farnham <brythonick@gmail.com>
2024-02-25 12:37:59 +0000

    Add initial working version of main program
    Reads the given sample and writes the earliest and latest dates to the
    console. It would be nice to neaten this up by taking the path and
    spec as program args instead. And implement configs for other log types
    to show ways to handle different formats.

--- LogAnalyser/LogReader/LogReader.cs
+++ LogAnalyser/LogReader/LogReader.cs
@@ -2,16 +2,17 @@
 
 namespace LogAnalyser
 {
-    public class LogReader
+	public class LogReader
 	{
 		private string _specificationName;
 		private LogSpec _spec;
+		private DateRecorder _dateRecorder = new();
 
 		public LogReader(string specificationName)
 		{
 			_specificationName = specificationName;
-            using FileStream stream = File.OpenRead("./Config/log_specification.json");
-            LogSpecifications? _specs = JsonSerializer.Deserialize<LogSpecifications>(stream);
+			using FileStream stream = File.OpenRead("./Config/log_specification.json");
+			LogSpecifications? _specs = JsonSerializer.Deserialize<LogSpecifications>(stream);
 			if (_specs == null)
 				throw new Exception("Could not read log specifications.");
 			else
@@ -19,7 +20,7 @@ namespace LogAnalyser
 				_spec = _specs.GetSpec(_specificationName);
 			}
 
-        }
+		}
 
 		public string SpecName { get { return _specificationName; } }
 		public LogSpec Spec { get { return _spec; } }
@@ -28,17 +29,31 @@ namespace LogAnalyser
 		{
 			if (_spec.delimiter != null)
 			{
-                string[] splitLine = logLine.Split(_spec.delimiter);
+				string[] splitLine = logLine.Split(_spec.delimiter);
 				logLine = splitLine[_spec.indicies.date];
-            }
+			}
 			LogDate date = new();
 			int yearLength = _spec.indicies.yearEnd - _spec.indicies.yearStart;
 			date.Year = uint.Parse(logLine.Substring(_spec.indicies.yearStart, yearLength + 1));
-            int monthLength = _spec.indicies.monthEnd - _spec.indicies.monthStart;
-            date.Month = uint.Parse(logLine.Substring(_spec.indicies.monthStart, monthLength + 1));
-            int dayLength = _spec.indicies.dayEnd - _spec.indicies.dayStart;
-            date.Day = uint.Parse(logLine.Substring(_spec.indicies.dayStart, dayLength + 1));
+			int monthLength = _spec.indicies.monthEnd - _spec.indicies.monthStart;
+			date.Month = uint.Parse(logLine.Substring(_spec.indicies.monthStart, monthLength + 1));
+			int dayLength = _spec.indicies.dayEnd - _spec.indicies.dayStart;
+			date.Day = uint.Parse(logLine.Substring(_spec.indicies.dayStart, dayLength + 1));
 			return date;
-        }
+		}
+
+		public void ParseSource(ILogSource source)
+		{
+			string? line = source.NextLine();
+			while (line != null)
+			{
+				LogDate date = ParseDate(line);
+				_dateRecorder.Add(date);
+				line = source.NextLine();
+			}
+		}
+
+		public LogDate EarliestDate() => _dateRecorder.Dates()[0];
+		public LogDate LatestDate() => _dateRecorder.Dates()[^1];
 	}
 }
--- LogAnalyser/Program.cs
+++ LogAnalyser/Program.cs
@@ -1,7 +1,7 @@
 ﻿using LogAnalyser;
 
 FileLogSource source = new("./Logs/ceredigion.txt");
-string? s1 = source.NextLine();
-string? s2 = source.NextLine();
-string? s3 = source.NextLine();
-string? s4 = source.NextLine();
+LogReader reader = new("ceredigion");
+reader.ParseSource(source);
+Console.WriteLine(reader.EarliestDate());
+Console.WriteLine(reader.LatestDate());
