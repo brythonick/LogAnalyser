42edc31ef23c8fc60da88be77d9010117711e32d
Nick Farnham <brythonick@gmail.com>
2024-02-25 16:57:48 +0000

    Add args to main program

--- LogAnalyser/Program.cs
+++ LogAnalyser/Program.cs
@@ -1,7 +1,29 @@
 ﻿using LogAnalyser;
 
-FileLogSource source = new("./Logs/ceredigion.txt");
-LogReader reader = new("ceredigion");
-reader.ParseSource(source);
-Console.WriteLine(reader.EarliestDate());
-Console.WriteLine(reader.LatestDate());
+try
+{
+    string module;
+    string specification;
+    string path;
+    module = args[0];
+    specification = args[1];
+    path = args[2];
+    if (module != "dates")
+    {
+        Console.WriteLine("Error: Other modules are not yet implemented.");
+        Environment.Exit(2);
+    }
+    else
+    {
+        FileLogSource source = new(path);
+        LogReader reader = new(specification);
+        reader.ParseSource(source);
+        Console.WriteLine(reader.EarliestDate());
+        Console.WriteLine(reader.LatestDate());
+    }
+}
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("Error: One or more args missing.");
+    Environment.Exit(1);
+}
